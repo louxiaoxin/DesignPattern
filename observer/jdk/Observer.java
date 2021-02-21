c42a5227e3de618d83fad2f4e861c5b21a3b803a
louxiaoxin <790470307@qq.com>
2021-02-22 00:54:04 +0800

    Observer pattern in jdk

---
+++ observer/jdk/Observer.java
@@ -0,0 +1,9 @@
+package DesignPattern.observer.jdk;
+
+import java.util.Observable;
+
+public class Observer extends Observable {
+    public static void main(String[] args) {
+        Observable observable = new Observable();
+    }
+}
