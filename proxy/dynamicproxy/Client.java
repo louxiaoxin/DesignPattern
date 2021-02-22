c22bf7d30b21730558ab586560dfabdf6a03d924
louxiaoxin <790470307@qq.com>
2021-02-22 22:19:37 +0800

    dynamic proxy

--- proxy/dynamicproxy/Client.java
+++ proxy/dynamicproxy/Client.java
@@ -1,5 +1,11 @@
 package DesignPattern.proxy.dynamicproxy;
 
+/**
+ * 动态代理：
+ *     代理对象不需要实现接口，但是目标对象要实现接口，否则不能用动态代理
+ *     代理对象的生成，是利用JDK的API，动态的在内存中构建代理对象
+ *     动态代理也叫：JDK代理，接口代理
+ */
 public class Client {
     public static void main(String[] args) {
         ITeacherDao iTeacherDao = new TeacherDao();
