5e4fb7b9c6d3c1343b717d8c2887bc5f4f1537cc
louxiaoxin <790470307@qq.com>
2021-02-22 22:33:46 +0800

    cglib proxy

---
+++ proxy/cglibproxy/ProxyFactory.java
@@ -0,0 +1,30 @@
+package DesignPattern.proxy.cglibproxy;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class ProxyFactory implements MethodInterceptor {
+    private Object target;
+
+    public ProxyFactory(Object target) {
+        this.target = target;
+    }
+
+    public Object getProxyInstance() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(target.getClass());
+        enhancer.setCallback(this);
+        return enhancer.create();
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("cglib代理开始");
+        Object returnObj = method.invoke(target, objects);
+        System.out.println("cglib代理结束");
+        return returnObj;
+    }
+}
---
+++ proxy/cglibproxy/TeacherDao.java
@@ -0,0 +1,7 @@
+package DesignPattern.proxy.cglibproxy;
+
+public class TeacherDao {
+    public void teach() {
+        System.out.println("cglib: 老师授课中。。。");
+    }
+}
