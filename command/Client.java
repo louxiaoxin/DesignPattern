b95a5cd9bcbbf824bb76c01ec510e0bd32e86c80
louxiaoxin <790470307@qq.com>
2021-02-23 23:13:52 +0800

    command

--- command/Client.java
+++ command/Client.java
@@ -5,10 +5,17 @@ public class Client {
         LightReceiver lightReceiver = new LightReceiver();
         LightOnCommand lightOnCommand = new LightOnCommand(lightReceiver);
         LightOffCommand lightOffCommand = new LightOffCommand(lightReceiver);
+        TVReceiver tvReceiver = new TVReceiver();
+        TVOffCommand tvOffCommand = new TVOffCommand(tvReceiver);
+        TVOnCommand tvOnCommand = new TVOnCommand(tvReceiver);
         RemoteController remoteController = new RemoteController();
         remoteController.setCommand(0, lightOnCommand, lightOffCommand);
+        remoteController.setCommand(1, tvOnCommand, tvOffCommand);
         remoteController.onButtonWasPushed(0);
         remoteController.offButtonWasPushed(0);
+        remoteController.undoButtonWasPushed();
+        remoteController.onButtonWasPushed(1);
+        remoteController.offButtonWasPushed(1);
         remoteController.undoButtonWasPushed();
     }
 }
---
+++ command/TVOffCommand.java
@@ -0,0 +1,19 @@
+package DesignPattern.command;
+
+public class TVOffCommand implements Command {
+    public TVReceiver tvReceiver;
+
+    public TVOffCommand(TVReceiver tvReceiver) {
+        this.tvReceiver = tvReceiver;
+    }
+
+    @Override
+    public void execute() {
+        tvReceiver.off();
+    }
+
+    @Override
+    public void undo() {
+        tvReceiver.on();
+    }
+}
